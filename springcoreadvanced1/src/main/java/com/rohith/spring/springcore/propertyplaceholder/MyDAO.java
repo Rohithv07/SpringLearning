22588ec1b5a791412ba6d06c068119958366aff3
vazha <rohithv63@gmail.com>
2020-07-16 12:23:54 +0530

    Practiced property placeholder

---
+++ springcoreadvanced1/src/main/java/com/rohith/spring/springcore/propertyplaceholder/MyDAO.java
@@ -0,0 +1,13 @@
+package com.rohith.spring.springcore.propertyplaceholder;
+
+public class MyDAO {
+	private String dbServer;
+	MyDAO(String dbServer){
+		this.dbServer=dbServer;
+	}
+	@Override
+	public String toString() {
+		return "MyDAO [dbServer=" + dbServer + "]";
+	}
+	
+}
